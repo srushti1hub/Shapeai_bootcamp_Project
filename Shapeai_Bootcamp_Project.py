eebb84bcc7fb20ee8e1320dd7195a31905584681
Srushti Rathva <76732482+srushti1hub@users.noreply.github.com>
2021-06-22 19:38:03 +0530

    Update Shapeai_Bootcamp_Project.py

--- Shapeai_Bootcamp_Project.py
+++ Shapeai_Bootcamp_Project.py
@@ -1,11 +1,11 @@
 import requests
 import json
 from datetime import datetime
-API_Key = '6f0a694a7db072a42e1a932827f54997'
+API_Key = '8918cd3f85ebd5f347b6f4eeb7c60b43'
 #Input from the user for loaction
 location = input('Enter the city : ')
-Complete_API_Link = "api.openweathermap.org/data/2.5/weather?q="+location+"&appid"+API_Key
-API_Link = requests.get(Complete_API_Link)
+Complete_API_Link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+API_Key
+API_Link = requests.get(Complete_API_Link,'allow_redirects=True')
 API_Data = API_Link.json()
 #Longitude of Location
 longitude = API_Data['coord']['lon']
@@ -21,19 +21,15 @@ humidity = API_Data['main']['humidity']
 pressure = API_Data['main']['pressure']
 #Wind Speed at Location
 wind_speed = API_Data['wind']['speed']
-#Wind Direction at Location
-wind_direction = API_Data['main']['deg']
 today = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
 #Displaying All Information on Console
-print("Entered City : "+location)
-print("Longitude : "+longitude+"  Latitude : "+latitude)
-print("Temperature    : "+temp+ " C")
-print("Description    : "+description)
-print("Humidity       : "+humidity+" %")
-print("Pressure       :"+pressure+" hPa")
-print("Wind Speed     : "+wind_speed+" m/s")
-print("Wind Direction :"+wind_direction+" degrees")
+print("Entered City   : "+location)
+print("Longitude      : ",longitude,"\nLatitude       : ",latitude)
+print("Temperature    : ",temp, "C")
+print("Description    : ",description)
+print("Humidity       : ",humidity,"%")
+print("Pressure       : ",pressure,"hPa")
+print("Wind Speed     : ",wind_speed,"m/s")
 #Recording the Information in txt format
 #The file will be downloaded in PC
-with open('Weatherinfo.txt','wb') as f:
-    f.write(API_Link.content)
+open('Weatherinfo.txt','wb').write(API_Link.content)
